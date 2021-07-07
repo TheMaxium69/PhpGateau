a822c412e0d4b681b2e03d35cb5bf4b17126d42b
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-07 09:53:47 +0200

    pdo update

--- core/Database.php
+++ core/Database.php
@@ -1,18 +1,33 @@
 <?php
 class Database 
 {
+    private static $instancePDO = null;
 
     public static function getPdo(){
 
-        $pdo = new PDO('mysql:host=localhost;dbname=garages','garage' ,'garage', [
+        if(self::$instancePDO === null){
+            self::$instancePDO = new PDO('mysql:host=localhost;dbname=garages','garage' ,'garage', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                PDO::ATTR_PERSISTENT
+            ]);
+        }
+
+        return self::$instancePDO;
+
+    }
+
+    /*
+    public static function getPdo(){
+
+            $PDO = new PDO('mysql:host=localhost;dbname=garages','garage' ,'garage', [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 PDO::ATTR_PERSISTENT
             ]);
 
-          return $pdo;
-
-    }
+        return $PDO;
+    }*/
 }
 
 
